73fe141e335c96586a347991d00a3b0cd7ee8933
Andrei Korolev <andrewsalt.e@gmail.com>
2022-07-15 16:03:31 +0500

    feat(updates/base): require `get_state_key` implementation

--- telefone_types/updates/base.py
+++ telefone_types/updates/base.py
@@ -1,4 +1,6 @@
 import typing
+from abc import abstractmethod
+
 from pydantic import BaseModel
 
 from telefone_types.states import StatePeer
@@ -10,6 +12,10 @@ if typing.TYPE_CHECKING:
 class BaseBotUpdate(BaseModel):
     state_peer: typing.Optional[StatePeer] = None
     unprepared_ctx_api: typing.Optional[typing.Any] = None
+
+    @abstractmethod
+    def get_state_key(self) -> typing.Optional[int]:
+        pass
 
     @property
     def ctx_api(self) -> typing.Optional[typing.Union["ABCAPI", "API"]]:
